1395aa94f723a32b95c9b4f7a260456824ab1a4d
Charles Darling <cdarlingiv@gmail.com>
2024-05-01 19:19:45 -0400

    add footer, update rout links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Rates from './Rates';
+import Convert from './Convert';
+import Charts from './Charts';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 import './App.css';
-
-const Home = () => {
-  return <h2>Home</h2>;
-}
-
-const About = () => {
-  return <h2>About</h2>;
-}
-
-const Contact = () => {
-  return <h2>Contact</h2>;
-}
 
 const NotFound = () => {
   return <h2>404 Not Found</h2>;
@@ -22,27 +17,38 @@ const App = () => {
   return (
     <Router>
       <div className="container">
-        <h2>React Rotuer Demo</h2>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Rates</Link>
-            </li>
-            <li>
-              <Link to="/about/">Convert</Link>
-            </li>
-            <li>
-              <Link to="/contact/">Charts</Link>
-            </li>
-          </ul>
+        <h2>Currency React App</h2>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link class="nav-item nav-link active" to="/">Rates<span class="sr-only">(current)</span></Link>
+              <Link class="nav-item nav-link" to="/convert/">Convert</Link>
+              <Link class="nav-item nav-link" to="/charts/">Charts</Link>
+            </div>
+          </div>
         </nav>
         <Switch>
           <Route path="/" exact component={Rates} />
-          <Route path="/about/" component={Convert} />
-          <Route path="/contact/" component={Charts} />
+          <Route path="/convert/" component={Convert} />
+          <Route path="/charts/" component={Charts} />
           <Route component={NotFound} />
         </Switch>
       </div>
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="copyright-box">
+                <p class="copyright"><span><FontAwesomeIcon icon={faFacebook} /><FontAwesomeIcon icon={faInstagram} /><FontAwesomeIcon icon={faTwitter} /></span><span>&copy; 2024 Copyright <strong>D4 Web Development LLC</strong>. All Rights Reserved</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </Router> 
   );
 }
