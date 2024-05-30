c2767b5c74310399a13fbbbb134f4fa8ccb9ad22
Charles Darling <cdarlingiv@gmail.com>
2024-05-29 22:08:59 -0400

    update Charts.js and index.css

--- src/Charts.js
+++ src/Charts.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import CurrencyDropdown from './Currency';
 import { json, checkStatus } from './utils';
-import Chart from 'chart.js/auto';
+import Chart from 'chart.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotate } from '@fortawesome/free-solid-svg-icons';
 
 class Charts extends React.Component {
   constructor(props) {
@@ -42,7 +44,7 @@ class Charts extends React.Component {
         }
         const chartLabels = Object.keys(data.rates);
         const chartData = chartLabels.map((date) => data.rates[date][base2]);
-        const chartLabel = `${base1}/${base2}`;
+        const chartLabel = `${base1} to ${base2}`;
         this.buildChart(chartLabels, chartData, chartLabel);
       })
       .catch((error) => this.setState({ error: error.message }));
@@ -101,6 +103,35 @@ class Charts extends React.Component {
     });
   };
 
+  swapBases = () => {
+    this.setState((prevState) => ({
+      base1: prevState.base2,
+      base2: prevState.base1,
+      dropdownKey: prevState.dropdownKey + 1 // Update the key to force remount
+    }), () => {
+      const { startDate, endDate } = this.state; // Fetch startDate and endDate here
+      this.fetchData(this.state.base1, this.state.base2, startDate, endDate);
+    });
+  };
+
+  setDateRange = (days) => {
+    const endDate = new Date().toISOString().split('T')[0];
+    const startDate = new Date(new Date().getTime() - days * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
+    this.setState({ startDate, endDate }, () => {
+      const { base1, base2 } = this.state;
+      this.fetchData(base1, base2, startDate, endDate);
+    });
+  };
+
+  setYTD = () => {
+    const endDate = new Date().toISOString().split('T')[0];
+    const startDate = new Date(new Date().getFullYear(), 0, 1).toISOString().split('T')[0];
+    this.setState({ startDate, endDate }, () => {
+      const { base1, base2 } = this.state;
+      this.fetchData(base1, base2, startDate, endDate);
+    });
+  };
+
   render() {
     const { error, base1, base2 } = this.state;
 
@@ -111,16 +142,31 @@ class Charts extends React.Component {
     return (
       <div className='rateContainer'>
         <h3 className='text-center'>Historical Currency Data</h3>
-        <canvas ref={this.chartRef} />
-
+        <div className='canvasBox'>
+          <canvas ref={this.chartRef} />
+        </div>
         <div className='row my-5 py-2'>
           <div className='col-lg-5 d-flex justify-content-center align-items-center'>
             <h4>From: </h4>
             <CurrencyDropdown onBaseChange={this.handleBaseChange1} defaultValue={base1} />
           </div>
+          <div className='col-lg-2 d-flex justify-content-center align-items-center'>
+            <FontAwesomeIcon icon={faRotate} onClick={this.swapBases} style={{ cursor: 'pointer' }} />
+          </div>
           <div className='col-lg-5 d-flex justify-content-center align-items-center'>
             <h4>To: </h4>
             <CurrencyDropdown onBaseChange={this.handleBaseChange2} defaultValue={base2} />
+          </div>
+        </div>
+        <div className='row my-5 py-2'>
+          <div className='col-lg-4 d-flex justify-content-center align-items-center'>
+            <button className='btn btn-primary' onClick={() => this.setDateRange(30)}>Last 30 Days</button>
+          </div>
+          <div className='col-lg-4 d-flex justify-content-center align-items-center'>
+            <button className='btn btn-primary' onClick={this.setYTD}>Year to Date</button>
+          </div>
+          <div className='col-lg-4 d-flex justify-content-center align-items-center'>
+            <button className='btn btn-primary' onClick={() => this.setDateRange(365)}>Last 1 Year</button>
           </div>
         </div>
       </div>
